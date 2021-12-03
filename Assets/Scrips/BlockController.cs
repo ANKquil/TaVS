a503ad71e3523a878a429746bd525314cf690b9f
ANKquil <thekolinfell@gmail.com>
2021-12-03 05:45:48 +0300

    Complite BlockController logic in Game

---
+++ Assets/Scrips/BlockController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public GameObject krestik;
+    public GameObject nolik;
+
+    bool active = false;
+
+    public void SetActiveBlock(int i)
+    {
+        if (i == 1)
+        {
+            krestik.SetActive(true);
+            active = true;
+        }
+        else if (i == 2)
+        {
+            nolik.SetActive(true);
+            active = true;
+        }
+        else
+        {
+            krestik.SetActive(false);
+            nolik.SetActive(false);
+            active = false;
+        }
+    }
+
+    public bool IsActive()
+    {
+        return active;
+    }
+}
